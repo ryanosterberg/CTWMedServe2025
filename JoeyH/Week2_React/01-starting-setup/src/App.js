e8a905d58594998201f90fa1c8577048ed266629
Joey <joeyhansen221@gmail.com>
2025-06-11 14:01:19 -0500

    ExpenseItem.js and .css added

--- JoeyH/Week2_React/01-starting-setup/src/App.js
+++ JoeyH/Week2_React/01-starting-setup/src/App.js
@@ -1,7 +1,12 @@
+import ExpenseItem from "./Components/ExpenseItem";
+
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>This is also visable</p>
+      <ExpenseItem/>
     </div>
   );
 }
