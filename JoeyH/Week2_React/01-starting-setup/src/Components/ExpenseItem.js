f26078e45bb4d11ff22016ffbe76f4b59b926171
Joey <joeyhansen221@gmail.com>
2025-06-11 14:18:52 -0500

    Data Driven Expense Item

--- JoeyH/Week2_React/01-starting-setup/src/Components/ExpenseItem.js
+++ JoeyH/Week2_React/01-starting-setup/src/Components/ExpenseItem.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28);  
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
     return (
         <div>
-            <div className='expense-item'>June 10th, 2022</div>
+            <div className='expense-item'>
+                    <div>{expenseDate.toISOString()}</div>
+                </div>
             <div className='expense-item__description'>
-                <h2>Car Insurance!</h2>
-                <div className="expense-item__price">$294.67</div>
+                <h2>{expenseTitle}</h2>
+            <div className="expense-item__price">{expenseAmount}</div>
             </div>
         </div>
     );
