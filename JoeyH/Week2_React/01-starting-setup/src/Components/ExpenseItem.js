894b3aa7b7c0975ec979758fb3c88e7c7ac2d7d3
Joey <joeyhansen221@gmail.com>
2025-06-11 14:11:56 -0500

    ExpenseItem updated

--- JoeyH/Week2_React/01-starting-setup/src/Components/ExpenseItem.js
+++ JoeyH/Week2_React/01-starting-setup/src/Components/ExpenseItem.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
     return (
-        <div><h2>Expense Item!</h2></div>
+        <div>
+            <div className='expense-item'>June 10th, 2022</div>
+            <div className='expense-item__description'>
+                <h2>Car Insurance!</h2>
+                <div className="expense-item__price">$294.67</div>
+            </div>
+        </div>
     );
     
 }
