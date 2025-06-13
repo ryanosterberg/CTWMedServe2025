e03b069b5e427a8499f206b8091bb21cfbc865c8
Joey <joeyhansen221@gmail.com>
2025-06-13 12:43:50 -0500

    Composition

--- JoeyH/Week2_React/01-starting-setup/src/Components/Expenses.js
+++ JoeyH/Week2_React/01-starting-setup/src/Components/Expenses.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./ExpenseDate.css";
-
+import Card from "./Card";
 
 function Expenses(props) {
 
     return (
-        <div className="expenses">
+        <Card className="expenses">
         <ExpenseItem 
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -24,7 +24,7 @@ function Expenses(props) {
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}/>
-        </div>
+        </Card>
     );
 }
 
